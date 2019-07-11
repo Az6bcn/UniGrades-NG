7bc6dd8b2ff40cad5f48a1d3802f6057df8dfcec
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-11 20:36:37 +0100

    Angular Architecture

---
+++ src/app/Features/subject-marks/subject-marks.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SubjectMarksRoutingModule } from './subject-marks-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SubjectMarksRoutingModule
+  ]
+})
+export class SubjectMarksModule { }
