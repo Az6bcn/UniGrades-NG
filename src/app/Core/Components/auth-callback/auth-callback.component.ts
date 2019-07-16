bb0da6b7f081d611fe94c0e528107755f794d355
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-16 22:25:43 +0100

    control auth call back component to  show spinner while signing in user

--- src/app/Core/Components/auth-callback/auth-callback.component.ts
+++ src/app/Core/Components/auth-callback/auth-callback.component.ts
@@ -1,3 +1,4 @@
+import { BehaviorSubject } from 'rxjs';
 import { AuthenticationService } from './../../Services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,17 +11,22 @@ import { IfStmt } from '@angular/compiler';
   styleUrls: ['./auth-callback.component.css']
 })
 export class AuthCallbackComponent implements OnInit {
-returnURL: string;
-  constructor(private authService: AuthenticationService,
-              private activatedRoute: ActivatedRoute,
-              private notifierService: NotifierService,
-              private router: Router) { }
+
+  isLoading$ = new BehaviorSubject<boolean>(false);
+  returnURL: string;
+  constructor(
+    private authService: AuthenticationService,
+    private activatedRoute: ActivatedRoute,
+    private notifierService: NotifierService,
+    private router: Router) { }
 
   async ngOnInit() {
     // when route is active, check if the fragment # doesn,t contain error
-    if (this.activatedRoute.snapshot.fragment.indexOf('error') >= 0 ) {
-        this.notifierService.notify('error', 'Authentication not successful...... Please, try again later');
+    if (this.activatedRoute.snapshot.fragment.indexOf('error') >= 0) {
+      this.notifierService.notify('error', 'Authentication not successful...... Please, try again later');
     }
+
+    this.isLoading$.next(true);
 
     // process and complete the authentication by processing what the authServer has come back with
     const user = await this.authService.processAuthServerCallbackResponse();
@@ -31,9 +37,11 @@ returnURL: string;
       if (returnURL) {
         sessionStorage.removeItem('returnURL');
       }
+      this.isLoading$.next(false);
     }
     else {
       this.router.navigate(['../default']);
+      this.isLoading$.next(false);
     }
 
   }
