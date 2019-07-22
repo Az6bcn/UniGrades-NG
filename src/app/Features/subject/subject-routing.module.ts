9feab453045f5c719d04aea42dafd2aa9d4d3210
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-22 08:28:28 +0100

    modified route

--- src/app/Features/subject/subject-routing.module.ts
+++ src/app/Features/subject/subject-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoursesComponent } from './Components/courses/courses.component';
 import { CoursesGradesComponent } from './Components/courses-grades/courses-grades.component';
+import { AuthGuard } from 'src/app/Core/Guards/auth.guard';
 
 
 const routes: Routes = [
+
+  { path: 'my-courses',
+    component: CoursesComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'courses-grades',
-    component: CoursesGradesComponent
-  },
-  { path: '',
-    component: CoursesComponent
+    component: CoursesGradesComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
