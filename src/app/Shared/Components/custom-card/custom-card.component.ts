02c0d41345faa092681a25c0339204b716be3033
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-23 07:53:27 +0100

    Added custom subject card component to shared

--- src/app/Shared/Components/custom-card/custom-card.component.ts
+++ src/app/Shared/Components/custom-card/custom-card.component.ts
@@ -1,6 +1,6 @@
 import { Subject } from '../../../Models/Subject';
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-custom-card',
@@ -9,10 +9,22 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CustomCardComponent implements OnInit {
 @Input() courses: Array<Subject>;
+@Output() editcourse = new EventEmitter<Subject>();
+@Output() addgrade = new EventEmitter<number>();
+@Output() deletecourse = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
     console.log(this.courses);
   }
-
+  addGrade(courseId: number) {
+    this.addgrade.emit(courseId);
+  }
+  editCourse(course: Subject) {
+    this.editcourse.emit(course);
+  }
+  deleteCourse(courseId: number) {
+    this.deletecourse.emit(courseId);
+  }
 }
