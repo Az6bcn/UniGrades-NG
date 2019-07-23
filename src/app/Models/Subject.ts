871759f25fb16ee9cdd5ea63cd6fc1f0386f6ea1
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-23 07:52:41 +0100

    added subject name to subject model

--- src/app/Models/Subject.ts
+++ src/app/Models/Subject.ts
@@ -1,6 +1,7 @@
 import { AcademicSemesterEnum } from './Enums/AcademicSemester.Enum';
 export class Subject {
   id: number;
+  subjectName: string;
   courseYear: Date;
   totalPercentage: number;
   userId: string;
