596b4d497a7e4315b644424bd08504f6fc9ff6b1
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-07-23 22:38:50 +0100

    Modified to show error message from api

--- src/app/Features/subject/Components/courses/courses.component.ts
+++ src/app/Features/subject/Components/courses/courses.component.ts
@@ -1,9 +1,12 @@
+import { Grade } from './../../../../Models/Grade';
+import { GradesService } from './../../../../Services/grades.service';
 import { BehaviorSubject } from 'rxjs';
 import { UserService } from './../../../../Core/Services/user.service';
 import { Subject } from './../../../../Models/Subject';
 import { SubjectsService } from './../../../../Services/subjects.service';
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-courses',
@@ -14,7 +17,9 @@ export class CoursesComponent implements OnInit {
 courses: Array<Subject>;
 isLoading$ = new BehaviorSubject<boolean>(true);
   constructor(private subjectService: SubjectsService,
-              private userService: UserService) { }
+              private userService: UserService,
+              private gradeService: GradesService,
+              private notifierService: NotifierService) { }
 
   ngOnInit() {
 
@@ -26,17 +31,56 @@ isLoading$ = new BehaviorSubject<boolean>(true);
       )
       .subscribe((response: Array<Subject>) => {
         this.courses = response;
-        console.log('courses ', response);
       });
   }
 
-  addGrade(courseId: number) {
+  addGrade(grade: Grade) {
 
+  }
+
+  addCourse(course: Subject, courseId: number) {
+    this.subjectService.AddSubject(course)
+    .pipe(
+      finalize(() => this.isLoading$.next(false))
+    )
+    .subscribe(response => {
+      if (response.success) {
+        this.notifierService.notify('success', 'Deleted Successfully');
+      }
+    },
+    error => {
+      this.notifierService.notify('success', error.error);
+    });
   }
   editCourse(course: Subject) {
-
+    this.subjectService.EditCourse(course)
+    .pipe(
+      finalize(() => this.isLoading$.next(false))
+    )
+    .subscribe(response => {
+      if (response.success) {
+        this.notifierService.notify('success', 'Course Edited Successfully');
+      }
+    },
+    error => {
+      this.notifierService.notify('success', error.error);
+    });
   }
   deleteCourse(courseId: number) {
+    this.subjectService.DeleteCourse(courseId)
+      .pipe(
+        finalize(() => this.isLoading$.next(false))
+      )
+      .subscribe(response => {
 
+          const index = this.courses.findIndex(x => x.id === courseId);
+          this.courses.splice(index, 1);
+
+          this.notifierService.notify('success', 'Deleted Successfully');
+
+      },
+      error => {
+        this.notifierService.notify('success', error.errorMessage);
+      });
   }
 }
