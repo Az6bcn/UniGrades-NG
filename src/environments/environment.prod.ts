c0488560236c158f0a6658d5cd1b77806f746de0
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2019-10-10 22:53:26 +0100

    copied dev environment configs to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  authBaseURL: 'http://172.16.172.157:5000',
+  baseURL: 'http://172.16.172.157:5001/api',
+  baseURL_String: '172.16.172.157:5001',
+  oidcClientSettings: {
+    authority: 'http://172.16.172.157:5000', // AuthServer URL, Identity Server URL (OIDC/OAuth2 provider)
+      client_id: 'unigrades-ng001', // client id as registered with Identity Server i.e OIDC/OAuth2 provider.
+      redirect_uri: 'http://localhost:4200/auth-callback', // The redirect URI to receive a response from Identity Server.
+      response_type: 'id_token token', // The type of response desired from Identity Server i.e OIDC/OAuth2 provider.
+      post_logout_redirect_uri: 'http://localhost:4200/signout-callback-oidc',
+      filterProtocolClaims: false,
+      loadUserInfo: true,
+      automaticSilentRenew: true,
+      scope: 'openid profile unigradesapi.full_access' // Scopes being requested from Identity Server i.e OIDC/OAuth2 provider.
 };
