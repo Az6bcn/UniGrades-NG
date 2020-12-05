346d8d45d5cfcedd1906c73f992644e9fed79f1d
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2020-12-05 15:32:12 +0000

    add todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,8 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 
-
+// TODO Remove code block:angular-notifier was throwing, Generic type 'ModuleWithProviders<T>' requires 1 type argument(s)
+// and failing in pipeline.
 declare module '@angular/core' {
   interface ModuleWithProviders<T = any> {
     ngModule: Type<T>;
